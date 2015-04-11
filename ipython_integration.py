fc7eed55d9eed4d492973d801de412476f7eabc1
Michele Silva <michele.s@icanlocalize.com>
2015-04-11 18:55:19 -0300

    Write dummy log to tmp dir

--- ipython_integration.py
+++ ipython_integration.py
@@ -1,7 +1,7 @@
 from pCore.LogFileWriter import XHTMLLogFileWriter
 from IPython.display import HTML
 from StringIO import StringIO
-htmlLogFile = XHTMLLogFileWriter(fileName='tmpfile')
+htmlLogFile = XHTMLLogFileWriter(fileName='/tmp/pdynamo_log')
 htmlLogFile.file = StringIO()
 
 # Display log as a table in IPython
@@ -10,7 +10,7 @@ def display_summary(pdynamo_object):
     value = htmlLogFile.file.getvalue()
     htmlLogFile.file.truncate(0)
     return HTML(value)
-    
+
 
 # Display molecules
 from pBabel import SMILES_FromSystem
