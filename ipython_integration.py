a2bc9557ba36f36ab060fe80a0e461cb26b95363
Michele Silva <michele.s@icanlocalize.com>
2015-04-12 15:08:36 -0300

    Allow for printing summary of lists

--- ipython_integration.py
+++ ipython_integration.py
@@ -4,23 +4,30 @@ from StringIO import StringIO
 htmlLogFile = XHTMLLogFileWriter(fileName='/tmp/pdynamo_log')
 htmlLogFile.file = StringIO()
 
+
 # Display log as a table in IPython
 def display_summary(pdynamo_object):
-    pdynamo_object.Summary(htmlLogFile)
-    value = htmlLogFile.file.getvalue()
+    value = ''
+    try:
+        items = iter(pdynamo_object)
+    except:
+        items = [pdynamo_object]
+    for item in items:
+        item.Summary(htmlLogFile)
+        value += htmlLogFile.file.getvalue()
     htmlLogFile.file.truncate(0)
     return HTML(value)
 
 
 # Display molecules
 from pBabel import SMILES_FromSystem
-
 from rdkit import Chem
 from rdkit.Chem.Draw import IPythonConsole
+
 
 def display_molecule(molecule):
     smiles_molecule = SMILES_FromSystem(molecule, log=None)
     return Chem.MolFromSmiles(smiles_molecule.replace('%', ''))
 
-#print water.connectivity.bonds.ToMapping()
-#print water.atoms.FormulaString()
+# print water.connectivity.bonds.ToMapping()
+# print water.atoms.FormulaString()
